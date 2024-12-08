ea9b4496af5b575ef4dbd4462766a87856185245
GauravNandgowli <me.nandgowligaurav@gmail.com>
2024-12-08 14:04:04 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import LandingPage from "./components/LandingPage"
+import Navbar from "./components/Navbar"
+import Ticker from "./components/Ticker"
+
+
+function App() {
+
+
+  return (
+    <>
+      <headers>
+        <Navbar></Navbar>
+        <LandingPage></LandingPage>
+        <Ticker></Ticker>
+      </headers>
+    </>
+  )
+}
+
+export default App
